177189b18c225f236d5878d12739de0f0bae57a8
deeperdesign <hellodeeper@gmail.com>
2018-05-15 15:26:01 +0200

    match making done

---
+++ week-02/day-2/matchmaking.ts
@@ -0,0 +1,27 @@
+'use strict';
+// Write a function that joins two array by matching one girl with one boy in a new array
+// Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+let girls: string[] = ['Eve', 'Ashley', 'Bözsi', 'Kat', 'Jane'];
+let boys: string[] = ['Joe', 'Fred', 'Béla', 'Todd', 'Neef', 'Jeff'];
+
+function makingMatches(groupOfGirls: any, groupOfBoys: any) {
+
+  let matches: string [] =[];
+
+  if (groupOfGirls.length > groupOfBoys.length) {
+    console.log(`Attention! There are ${groupOfGirls.length - groupOfBoys.length} girl without pair!`);
+  }
+  else if (groupOfGirls.length < groupOfBoys.length) {
+    console.log(`Attention! There are ${groupOfBoys.length - groupOfGirls.length} boy without pair!`);
+  }
+  for(let i: number=0; i < groupOfGirls.length; i++) {
+    matches.push(`${groupOfGirls[i]} - ${groupOfBoys[i]}`);
+  }
+
+  console.log(matches);
+
+}
+
+console.log(makingMatches(girls, boys));
+
+export = makingMatches;
