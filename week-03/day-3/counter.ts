49e6c9b40af81a3d23d275ee58a0775aa5fc443f
deeperdesign <hellodeeper@gmail.com>
2018-05-24 10:28:32 +0200

    counter

---
+++ week-03/day-3/counter.ts
@@ -0,0 +1,15 @@
+'use-strict';
+// Write a recursive function that takes one parameter: n and counts down from n.
+
+function countDown(n: number): number {
+    //this is my base case
+    if (n === 0) {
+      return 0;
+    }
+    else {
+        console.log(n); //returns the current value of n
+        return countDown(n - 1); //this is executed in every loop
+    }
+}
+  
+console.log(countDown(50));
