2794e63d3dbf7a14b6853aa67d7fab0807902d50
deeperdesign <hellodeeper@gmail.com>
2018-05-30 13:46:51 +0200

    import classes from instruments.ts a test app class implemented here

---
+++ week-04/day-3/app-instrument.ts
@@ -0,0 +1,35 @@
+'use-strict';
+
+import {Instrument} from './instruments';
+import {StringedInstrument} from './instruments';
+import {ElectricGuitar} from './instruments';
+import {BassGuitar} from './instruments';
+import {Violin} from './instruments';
+
+class app {
+    test():void {
+        
+        // Test cases
+
+        console.log('Test 1, create Electric Guitar, Bass Guitar and Violin with default strings.');
+        let guitar = new ElectricGuitar();
+        let bassGuitar = new BassGuitar();
+        let violin = new Violin();
+        
+        console.log('Test 1 Play');
+        guitar.play();
+        bassGuitar.play();
+        violin.play();
+        
+        console.log('Test 2, create Electric Guitar, Bass Guitar with 7 and 5 strings .');
+        let guitar2 = new ElectricGuitar(7);
+        let bassGuitar2 = new BassGuitar(5);
+        
+        console.log('Test 2 Play');
+        guitar2.play();
+        bassGuitar2.play();
+    }
+}
+
+let letsTest = new app();
+letsTest.test();
