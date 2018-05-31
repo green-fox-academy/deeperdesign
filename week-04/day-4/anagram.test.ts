d6f0ecff0478fd537038c35fe389a0ad5d914a5e
deeperdesign <hellodeeper@gmail.com>
2018-05-31 16:13:39 +0200

    anagrams shall be tested with this

---
+++ week-04/day-4/anagram.test.ts
@@ -0,0 +1,21 @@
+'use-strict';
+
+declare function require(path: string): any;
+const {test} = require('tape');
+
+import {anaGram} from './anagram';
+
+test('matcing anagrams', t => {
+    t.equal(anaGram('termelékenység','Megkéselt erény'),true);
+    t.end();
+});
+
+test('unmatching anagrams', t => {
+  t.equal(anaGram('termeléknység kalapács','Megkéselt erény'),false);
+  t.end();
+});
+
+test('one variable is missing', t => {
+  t.equal(anaGram('','Megkéselt erény'),false);
+  t.end();
+});
