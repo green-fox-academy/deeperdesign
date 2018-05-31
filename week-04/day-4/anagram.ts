bc09c03e3f8f84fd38c1a768227a3d8743456ea3
deeperdesign <hellodeeper@gmail.com>
2018-05-31 16:30:35 +0200

    improved to work even if the function is empty

--- week-04/day-4/anagram.ts
+++ week-04/day-4/anagram.ts
@@ -1,6 +1,6 @@
 'use-strict';
 
-export function anaGram(a: string, b: string):boolean {
+export function anaGram(a?: string, b?: string):boolean {
 
   //Function to filter out spaces
   function removeSpaces(element, index, array) { 
@@ -8,13 +8,16 @@ export function anaGram(a: string, b: string):boolean {
   }
 
   //Get variable a and b and convert it to lower case, then split its characters into an array, then sort them
-  let inputA = a.toLocaleLowerCase().split('').filter(removeSpaces).sort();
-  let inputB = b.toLocaleLowerCase().split('').filter(removeSpaces).sort();
+  if(a && b) {
+    let inputA = a.toLocaleLowerCase().split('').filter(removeSpaces).sort();
+    let inputB = b.toLocaleLowerCase().split('').filter(removeSpaces).sort();
 
-  if(inputA.toString() === inputB.toString()){
-    return true;
-  }
-  else {
+    if(inputA.toString() === inputB.toString()){
+      return true;
+    } else {
+      return false;
+    }
+  } else {
     return false;
   }
 }
