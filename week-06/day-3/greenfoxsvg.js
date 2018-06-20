b21fdf3722d9c4b50070b3764e6cbed9dc0ca8c3
deeperdesign <hellodeeper@gmail.com>
2018-06-20 16:45:10 +0200

    tomato eyed fox

---
+++ week-06/day-3/greenfoxsvg.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const leftEye = document.querySelector('#left_eye');
+const rightEye = document.querySelector('#right_eye');
+
+leftEye.setAttribute('fill', 'tomato');
+rightEye.setAttribute('fill', 'tomato');
