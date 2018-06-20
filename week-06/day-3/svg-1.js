cb8434403e6a79ba928810ec00c17d1387b591d2
deeperdesign <hellodeeper@gmail.com>
2018-06-20 16:38:19 +0200

    svg object modifyed

---
+++ week-06/day-3/svg-1.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const myRectangle = document.querySelector('#very_rectangle');
+
+myRectangle.setAttribute('x', '50');
+myRectangle.setAttribute('y', '50');
+myRectangle.style.fill = 'tomato';
