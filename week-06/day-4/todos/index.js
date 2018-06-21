14dd6f6f39f5d2d179650eed5fb71dba439d9e59
deeperdesign <hellodeeper@gmail.com>
2018-06-21 14:43:05 +0200

    todos with empty array handling

---
+++ week-06/day-4/todos/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// home page
+app.get('/', (req, res) => {
+
+  const todos = [
+    'get up',
+    'survive',
+    'go back to bed',
+  ];
+
+  // render `home.ejs`
+  res.render('home', {
+    title: 'Todos',
+    todos,
+  });
+});
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
