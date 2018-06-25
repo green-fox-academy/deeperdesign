17d5608c1c48432ac83ac8dc57e8152f518510ad
deeperdesign <hellodeeper@gmail.com>
2018-06-25 17:56:10 +0200

    do until implemeneted - / without what not handled /

--- week-07/day-1/frontend/routes.js
+++ week-07/day-1/frontend/routes.js
@@ -5,6 +5,7 @@ const app = express();
 const path = require('path');
 
 //Let app use static folder
+app.use(express.json());
 app.use('/wwwroot', express.static('wwwroot'));
 
 //Defining root
@@ -46,10 +47,7 @@ app.get('/api/greeter', (req, res) => {
 //greeter endpoint
 app.get('/api/appenda/:appendable', (req, res) => {
 
-  let provided = false;
-
   if (req.params.appendable) {
-    let provided = true;
     res.json({
       appended: `${req.params.appendable}a`,
     });
@@ -60,6 +58,50 @@ app.get('/api/appenda', (req, res) => {
   res.json(404, {
     message: 'error',
   });
+});
+
+//do until
+app.post('/api/dountil/:what', (req, res) => {
+  if (req.params.what === 'sum' && req.body.until) {
+    let sum = 0;
+    for (let i = 0; i <= req.body.until; i++) {
+      sum += i;
+    }
+    res.json({
+      result: sum,
+    });
+  }
+
+  if (req.params.what === 'factor' && req.body.until) {
+    function factorialize(num) {
+      if (num === 0 || num === 1)
+        return 1;
+      for (var i = num - 1; i >= 1; i--) {
+        num *= i;
+      }
+      return num;
+    };
+    res.json({
+      result: factorialize(req.body.until),
+    });
+  }
+  if (!req.body.until) {
+    res.json({
+      error: 'Please provide a number!',
+    });
+  }
+
+});
+
+app.post('/api/dountil', (req, res) => {
+  res.json({
+    error: 'Please provide a number!',
+  });
+  if (!req.params) {
+    res.json({
+      error: 'Please provide a number!',
+    });  
+  }
 });
 
 //export it for tape
