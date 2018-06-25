b46e9ab373a3ae51586fb854f78a04579036e69d
deeperdesign <hellodeeper@gmail.com>
2018-06-25 14:30:31 +0200

    Doubling implemented with error handling

--- week-07/day-1/frontend/routes.js
+++ week-07/day-1/frontend/routes.js
@@ -12,5 +12,19 @@ app.get('/', (req, res) => {
   res.sendfile(path.join(__dirname, './wwwroot/index.html'));
 });
 
+//doubling endpoint
+app.get('/api/doubling', (req, res) => {
+  if(req.query.input) {
+    res.json({
+      "received": req.query.input,
+      "result": req.query.input*2,
+    });
+  } else {
+    res.json({
+      "error": "Please provide an input!",
+    });
+  }
+});
+
 //export it for tape
 module.exports = app;
