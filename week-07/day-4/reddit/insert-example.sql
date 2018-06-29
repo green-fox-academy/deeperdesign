1cc1ea1473f9470d34724519eb04ca2fbc735e7b
deeperdesign <hellodeeper@gmail.com>
2018-06-29 13:50:01 +0200

    sample sql updated image links added, syntax shortened

--- week-07/day-4/reddit/insert-example.sql
+++ week-07/day-4/reddit/insert-example.sql
@@ -1,1 +1,1 @@
-INSERT INTO `reddit`.`posts` (`title`, `url`, `owner`) VALUES ('Post title sample1', 'http://default.com/example1', 'Johnie Doe');
+INSERT INTO posts (title, url, owner) VALUES ('Post title sample1', 'http://default.com/example1', 'Johnie Doe');
--- week-07/day-4/reddit/sample-data.sql
+++ week-07/day-4/reddit/sample-data.sql
@@ -4,6 +4,8 @@ LIMIT 0, 1000
 
 -- Date: 2018-06-28 10:45
 */
-INSERT INTO `posts` (`id`,`title`,`url`,`timestamp`,`score`,`owner`,`vote`) VALUES (1,'Post title sample1','http://default.com/example1','2018-06-28 10:27:51',0,'JohnieDoe',0);
-INSERT INTO `posts` (`id`,`title`,`url`,`timestamp`,`score`,`owner`,`vote`) VALUES (2,'Post title sample2','http://default.com/example2','2018-06-28 10:29:32',0,'TingTong',0);
-INSERT INTO `posts` (`id`,`title`,`url`,`timestamp`,`score`,`owner`,`vote`) VALUES (3,'Post title sample3','http://default.com/example3','2018-06-28 10:30:19',0,'DingDong',0);
+INSERT INTO posts (title, url, owner) VALUES ('Post title sample1', 'https://hdqwalls.com/download/mount-everest-qhd-2880x1800.jpg', 'Gang Bang');
+INSERT INTO posts (title, url, owner) VALUES ('Post title sample2', 'https://www.tibettravel.org/assets/images/everest/everest-base-camp/ascend-the-summit-mount-everest.jpg', 'Ding Dong');
+INSERT INTO posts (title, url, owner) VALUES ('Post title sample3', 'http://1.bp.blogspot.com/-DBWhKDbo7RI/UHhhnU6qiJI/AAAAAAAAB-o/7uSQkbsd0L0/s1600/himalayas+landscape+nature.jpg', 'Ting Tong');
+INSERT INTO posts (title, url, owner) VALUES ('Post title sample4', 'http://2.bp.blogspot.com/_sD01Cbqoqb0/TNVMD7n3lfI/AAAAAAAAAkQ/hiX0SN-5nEg/s1600/nature-photography-wallpaper-nature.jpg', 'Johnie Doe');
+INSERT INTO posts (title, url, owner) VALUES ('Post title sample5', 'https://media-cdn.tripadvisor.com/media/photo-s/06/c0/49/0e/mt-qomolangma-nature.jpg', 'Daisy Dee');
