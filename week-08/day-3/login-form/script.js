98deadd100e4985d82e230a122a74f2cfc4873b2
deeperdesign <hellodeeper@gmail.com>
2018-07-04 14:01:39 +0200

    alt shift f

--- week-08/day-3/login-form/script.js
+++ week-08/day-3/login-form/script.js
@@ -7,26 +7,26 @@ const submit = document.querySelector('input[type="submit"]');
 pwfield[1].addEventListener('keyup', (e) => {
   let pw1 = pwfield[0].value;
   let pw2 = pwfield[1].value;
-  if(pw1 != pw2){
+  if (pw1 != pw2) {
     pwdiv.appendChild(div).textContent = `Passwords doesn't match, please try again!`;
     div.classList.add('nomatch');
-    submit.setAttribute('disabled','');
+    submit.setAttribute('disabled', '');
   }
-  if(pw1 == pw2 && div.classList.contains('nomatch')) {
+  if (pw1 == pw2 && div.classList.contains('nomatch')) {
     pwdiv.removeChild(div);
     div.classList.remove('nomatch');
     submit.removeAttribute('disabled');
   }
 });
 
-reveal.addEventListener('click', function() {
-  if(pwdiv.classList.contains('clicked')){
-    pwfield[0].setAttribute('type','password');
-    pwfield[1].setAttribute('type','password');
+reveal.addEventListener('click', function () {
+  if (pwdiv.classList.contains('clicked')) {
+    pwfield[0].setAttribute('type', 'password');
+    pwfield[1].setAttribute('type', 'password');
     pwdiv.classList.remove('clicked');
   } else {
-    pwfield[0].setAttribute('type','text');
-    pwfield[1].setAttribute('type','text');
+    pwfield[0].setAttribute('type', 'text');
+    pwfield[1].setAttribute('type', 'text');
     pwdiv.classList.add('clicked');
   }
 })
